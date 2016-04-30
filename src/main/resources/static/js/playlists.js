6bfa686e67b94997ce0c96abc5ec6cc7c371cfad
Peter Clarke <PC1996@msn.com>
2016-04-30 17:19:44 -0400

    Reorganized the add event logic into a function and debugged some front-end issues

--- src/main/resources/static/js/playlists.js
+++ src/main/resources/static/js/playlists.js
@@ -1,6 +1,93 @@
 var name = "";
 var eventsArray = [];
 var currentEvent;
+var eventComparator = function(eventA, eventB) {
+	if (eventA == null || eventB == null) {
+		return -1;
+	}
+
+	var eventAStartTime = eventA.start;
+	var eventBStartTime = eventB.start;
+	
+	//Case where eventA and eventB are both in the AM or both PM
+	if (eventAStartTime.isAM && eventBStartTime.isAM 
+			|| (!eventAStartTime.isAM && !eventBStartTime.isAM)) {	
+		if (eventAStartTime.hour < eventBStartTime.hour) {
+			return -1;
+		} else if (eventAStartTime.hour > eventBStartTime.hour) {
+			return 1;
+		} else {
+			if (eventAStartTime.minute <= eventBStartTime.minute) {
+				return -1;
+			} else if (eventAStartTime.minute > eventBStartTime.minute) {
+				return 1;
+			}			
+		} 
+	
+	} else if (eventAStartTime.isAM && !eventBStartTime.isAM) {
+		//eventA comes before eventB
+		return -1;	
+	//Case where eventA is in the PM and eventA is in the AM	
+	} else {
+		//eventA comes after eventB
+		return 1;
+	}
+}
+
+
+if (window.location.pathname === "/playlists") {
+	$("#editEventForm").hide();
+	// First, set the logout link 
+	$("#logoutLink").attr('href', "http://localhost:" + window.location.port + "/vibe");
+
+	// Next, retrieve the user's code to send to the back-end from the URL	
+	var uri = new URI(window.location.href);
+	var urlParams = uri.search(true);
+	var codeFromURL = urlParams.code;
+	var postParams = {code: JSON.stringify(codeFromURL)};
+
+	$.post("/code", postParams, function(responseJSON) {
+	  // Back-end sends back the user's display name
+	  var backendParams = responseJSON.split(",");
+
+	  if (backendParams === "") {
+	  	alert("Error loading user information. Please logout and login again!");
+	  }
+
+	  var username = backendParams[0].slice(1, backendParams[0].length).trim(); // remove the [
+	  var playlistURI = "https://embed.spotify.com/?uri=" 
+	  + backendParams[1].slice(0, backendParams[1].length - 1).trim(); // remove the ]
+
+	  document.cookie = "name=" + username; // Store the username in a cookie
+
+	  console.log("user: " + username);
+	  console.log("playlist: " + playlistURI);
+
+	  if (backendParams != null) {
+	    name = username;
+		$("#displayname").html(name);
+	  }
+
+	  // $("#playlist").attr('src', playlistURI);
+	
+	}); // end access token code post
+
+	
+	/* Handles adding an event */
+	$("#AddNewEvent").click(function() {
+		console.log("Adding new event...");
+		addEvent();
+    }); // end add new click handler
+
+	$(".anEvent").click(function() {
+		console.log("Clicked on an event");
+		var eventID = this.id;
+		console.log("Current event: " + currentEvent + "[id: " + eventID + "]");
+		createDropdown(eventID);
+	}); // end click on event handler
+
+}
+
 
 //Object to mirror EventTime from the backend
 function EventTime(eventTime) {
@@ -36,7 +123,6 @@ function renderCalander(event){
 				event.start.hour + timePeriod + " | " + event.name + " " +
 				"<i class='fa fa-fw fa-caret-down'></i></a>" +
 				"<ul id= 'demo'" + "class='collapse'>" +
-
 					"<li id='viewPlaylist'>" +
 						"<a href='#'>View Playlist" + event.start.hour + "</a>" +
 					"</li>" +
@@ -73,95 +159,24 @@ function renderCalander(event){
 toggle dropdown content using show and hide  */
 function createDropdown(id) {
     document.getElementById(id).classList.toggle("show");
-}
+    if (!event.target.matches('collapse')) {
 
-
-
-
-// <div id='view-playlist-panel'> ... </div>
-//	    <div id='customize-playlist-panel'> ... </div>
-//	    <div id='use-spotify-playlist-panel'> ... </div>
-//	    <div id='edit-event'> ... </div>
-
-
-
-var eventComparator = function(eventA, eventB) {
-	if (eventA == null || eventB == null) {
-		return -1;
-	}
-
-	var eventAStartTime = eventA.start;
-	var eventBStartTime = eventB.start;
-	
-	//Case where eventA and eventB are both in the AM or both PM
-	if (eventAStartTime.isAM && eventBStartTime.isAM 
-			|| (!eventAStartTime.isAM && !eventBStartTime.isAM)) {	
-		if (eventAStartTime.hour < eventBStartTime.hour) {
-			return -1;
-		} else if (eventAStartTime.hour > eventBStartTime.hour) {
-			return 1;
-		} else {
-			if (eventAStartTime.minute <= eventBStartTime.minute) {
-				return -1;
-			} else if (eventAStartTime.minute > eventBStartTime.minute) {
-				return 1;
-			}			
-		} 
-	
-	} else if (eventAStartTime.isAM && !eventBStartTime.isAM) {
-		//eventA comes before eventB
-		return -1;	
-	//Case where eventA is in the PM and eventA is in the AM	
-	} else {
-		//eventA comes after eventB
-		return 1;
+	    var dropdowns = document.getElementsByClassName('collapse');
+	    var i;
+	    for (i = 0; i < dropdowns.length; i++) {
+	      var openDropdown = dropdowns[i];
+	      if (openDropdown.classList.contains('show')) {
+	        openDropdown.classList.remove('show');
+	      }
+	    }
 	}
 }
 
-if (window.location.pathname === "/playlists") {
-	$("#editEventForm").hide();
-	// First, set the logout link 
-	$("#logoutLink").attr('href', "http://localhost:" + window.location.port + "/vibe");
-
-	// Next, retrieve the user's code to send to the back-end from the URL	
-	var uri = new URI(window.location.href);
-	var urlParams = uri.search(true);
-	var codeFromURL = urlParams.code;
-	var postParams = {code: JSON.stringify(codeFromURL)};
-
-	$.post("/code", postParams, function(responseJSON) {
-	  // Back-end sends back the user's display name
-	  var backendParams = responseJSON.split(",");
-
-	  if (backendParams === "") {
-	  	alert("Error loading user information. Please logout and login again!");
-	  }
-
-	  var username = backendParams[0].slice(1, backendParams[0].length).trim(); // remove the [
-	  var playlistURI = "https://embed.spotify.com/?uri=" 
-	  + backendParams[1].slice(0, backendParams[1].length - 1).trim(); // remove the ]
-
-	  document.cookie = "name=" + username; // Store the username in a cookie
-
-	  console.log("user: " + username);
-	  console.log("playlist: " + playlistURI);
-
-	  if (backendParams != null) {
-	    name = username;
-		$("#displayname").html(name);
-	  }
-
-	// $("#playlist").attr('src', playlistURI);
-	
-	}); // end code post
-
-	
-	/* Handle clicking an event */
-	
-	$("#AddNewEvent").click(function() {
-		console.log("Adding new event...");
-
-		// necessary for some browser problems (saw on jquery's website)
+/**
+ * Adds an event to the list of events on the front-end
+ */
+function addEvent() {
+	// necessary for some browser problems (saw on jquery's website)
 		$.valHooks.textarea = {
 		  get: function( elem ) {
 		    return elem.value.replace( /\r?\n/g, "\r\n" );
@@ -173,28 +188,22 @@ if (window.location.pathname === "/playlists") {
 		var endTime = $('#endTime').val();
 		var startAP;
 		var endAP;
-		console.log("Event name: " + eventName);
-		console.log("Start time: " + startTime);
-		console.log("End time: " + endTime);
-		
-		console.log("Check start time value: " + $('#startAM').is(':checked'));
-		console.log("Check end time value: " + $('#endAM').is(':checked'));
 
+		// Check to see if start time is AM or PM
 		if ($('#startAM').is(':checked')) {
 			startAP = true;
 		} else {
 			startAP = false;
 		}
 		
+		// Check to see if end time is AM or PM
 		if ($('#endAM').is(':checked')) {
 			endAP = true;
 		} else {
 			endAP = false;
 		}
-		
-		console.log("start AM/PM Option: " + startAP);
-		console.log("end AM/PM Option: " + endAP);
-		
+
+		// Set up the event format and time format
 		eventFormat = /^[a-zA-Z]+$/;
 		timeFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
 		
@@ -208,59 +217,30 @@ if (window.location.pathname === "/playlists") {
 	    } else if(endTime == '' && !startTime.match(timeFormat)) {
 	    	alert("Invalid End Time: " + endTime);
 	    } else {
+	    	var postParameters = {
+				start : startTime ,
+				end : endTime ,
+				startAMPM : startAP ,
+				endAMPM : endAP,
+				name : eventName
+	    	};
 	    	
-    	var postParameters = {
-    			start : startTime ,
-    			end : endTime ,
-    			startAMPM : startAP ,
-    			endAMPM : endAP,
-    			name : eventName
-    	};
-    	
-    	$.post("/newEvent", postParameters, function(response) {
-    		//1. send stuff to back end and store in responseObject
-    		var responseObject = JSON.parse(response);
-    		console.log("Parsed json event: " +  responseObject);
-    		//2. Make calendar event object from responseObject
-    		var newEvent = new CalendarEvent(responseObject);
-    		currentEvent = newEvent;
-    		
-    		//3. Add new calendar event to user's list
-    		eventsArray.push(newEvent);
-    		
-    		//4. sort the list 
-    		eventsArray.sort(eventComparator);
-    		
-    		//5. Render calendar 
-    		// console.log("New Event Name : " + newEvent.name);
-    		// console.log("New Event Start Time Hour: " + newEvent.start.hour);
-    		// console.log("New Event Start Time AM: " + newEvent.start.isAM);
-    		// console.log("New Event End Time Hour: " + newEvent.end.hour);
-    		// console.log("New Event End Time AM: " + newEvent.end.isAM);
-    		// console.log("New Event : " + newEvent.id);
-    		renderCalander(currentEvent);
-    		
-    	}); // end post event
+	    	$.post("/newEvent", postParameters, function(response) {
+	    		// 1. send stuff to back end and store in responseObject
+	    		var responseObject = JSON.parse(response);
 
-	$(".anEvent").click(function() {
-		console.log("Clicked on an event");
-		var eventID = this.id;
-		console.log("Current event: " + currentEvent + "[id: " + eventID + "]");
-		createDropdown(eventID);
-		if (!event.target.matches('collapse')) {
-
-		    var dropdowns = document.getElementsByClassName('collapse');
-		    var i;
-		    for (i = 0; i < dropdowns.length; i++) {
-		      var openDropdown = dropdowns[i];
-		      if (openDropdown.classList.contains('show')) {
-		        openDropdown.classList.remove('show');
-		      }
-		    }
+	    		// 2. Make calendar event object from responseObject
+	    		var newEvent = new CalendarEvent(responseObject);
+	    		currentEvent = newEvent;
+	    		
+	    		// 3. Add new calendar event to user's list
+	    		eventsArray.push(newEvent);
+	    		
+	    		// 4. sort the list 
+	    		eventsArray.sort(eventComparator);
+	    		
+	    		//5. Render calendar
+	    		renderCalander(currentEvent);
+	    	});
 		}
-	});
-
-	    }
-	    
-		});
 }

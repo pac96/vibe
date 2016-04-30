e8a8c72d3499eb7172b012dd1985e22a3595c042
Chelse Steele <chelse-amoy_steele@brown.edu>
2016-04-30 16:48:49 -0400

    options js updates

--- src/main/resources/static/js/options.js
+++ src/main/resources/static/js/options.js
@@ -48,3 +48,66 @@ function getPlaylistURI(eventID) {
 	// TODO: retrieve the URI of a playlist given an event ID
 	return uri;
 }
+
+/**
+ * Displays the playlist for a given event.
+ */
+function displayCustomizePlaylist(eventID){
+    document.getElementById('view-playlist-panel').style.display = "block";   
+}
+
+/**
+ * Displays options for a users Spotify Playlist.
+ */
+function displaySpotifyPlaylist(eventID){
+    document.getElementById('customize-playlist-panel').style.display = "block";   
+
+}
+
+/**
+ * Allows a user to edit an event.
+ */
+function editEvent(eventID, toChange){
+	if (toChange) {
+        var currentEvent = document.getElementById('eventID');
+        if (currentEvent) {
+            var newHTML = 
+            	"<li id='" + event.id + "' class='eventClick' onclick='createDropdown()'> " +
+    			"<a href='#'>" +
+    			"<a href='javascript:;' data-toggle='collapse' data-target='#demo'>" +
+    				"<i class='fa fa-fw fa-arrows-v'></i> " +
+    				event.start.hour + timePeriod + " | " + event.name + " " +
+    				"<i class='fa fa-fw fa-caret-down'></i></a>" +
+    				"<ul id= 'demo'" + "class='collapse'>" +
+    					"<li>" +
+    						"<a href='#'>View Playlist" + event.name + "</a>" +
+    					"</li>" +
+    					"<li>" +
+    						"<a href='#'>Customize Playlist </a>" +
+    					"</li>" +
+    					"<li>" +
+    						"<a href='#'>Use Spotify Playlist </a>" +
+    					"</li>" +
+    					"<li>" +
+    						"<a href='#'>Edit Event </a>" +
+    					"</li>" +
+    					"<li>" +
+    						"<a href='#'>Delete Event </a>" +
+    					"</li>" +
+    				"</ul>" +
+    			"</a>" +
+    		"</li>";
+            elt.innerHTML = newHTML;
+        }
+    }
+    document.getElementById('use-spotify-playlist-panel').style.display = "block";   
+
+}
+
+/**
+ * Allows a user to delete an event.
+ */
+function deleteEvent(eventID){
+    document.getElementById('edit-event').style.display = "block";   
+
+}
--- src/main/resources/static/js/playlists.js
+++ src/main/resources/static/js/playlists.js
@@ -76,68 +76,6 @@ function createDropdown(id) {
 }
 
 
-/**
- * Displays the playlist for a given event.
- */
-function displayCustomizePlaylist(eventID){
-    document.getElementById('view-playlist-panel').style.display = "block";   
-}
-
-/**
- * Displays options for a users Spotify Playlist.
- */
-function displaySpotifyPlaylist(eventID){
-    document.getElementById('customize-playlist-panel').style.display = "block";   
-
-}
-
-/**
- * Allows a user to edit an event.
- */
-function editEvent(eventID, toChange){
-	if (toChange) {
-        var currentEvent = document.getElementById('eventID');
-        if (currentEvent) {
-            var newHTML = 
-            	"<li id='" + event.id + "' class='eventClick' onclick='createDropdown()'> " +
-    			"<a href='#'>" +
-    			"<a href='javascript:;' data-toggle='collapse' data-target='#demo'>" +
-    				"<i class='fa fa-fw fa-arrows-v'></i> " +
-    				event.start.hour + timePeriod + " | " + event.name + " " +
-    				"<i class='fa fa-fw fa-caret-down'></i></a>" +
-    				"<ul id= 'demo'" + "class='collapse'>" +
-    					"<li>" +
-    						"<a href='#'>View Playlist" + event.name + " onclick='displayPlaylist(" + event.id + ")' </a>" +
-    					"</li>" +
-    					"<li>" +
-    						"<a href='#'>Customize Playlist onclick='displayCustomizePlaylist(" + event.id + ")' </a>" +
-    					"</li>" +
-    					"<li>" +
-    						"<a href='#'>Use Spotify Playlist onclick='displaySpotifyPlaylist(" + event.id + ")'</a>" +
-    					"</li>" +
-    					"<li>" +
-    						"<a href='#'>Edit Event onclick='editEvent(" + event.id, true + ")'</a>" +
-    					"</li>" +
-    					"<li>" +
-    						"<a href='#'>Delete Event onclick='deleteEvent(" + event.id + ")'</a>" +
-    					"</li>" +
-    				"</ul>" +
-    			"</a>" +
-    		"</li>";
-            elt.innerHTML = newHTML;
-        }
-    }
-    document.getElementById('use-spotify-playlist-panel').style.display = "block";   
-
-}
-
-/**
- * Allows a user to delete an event.
- */
-function deleteEvent(eventID){
-    document.getElementById('edit-event').style.display = "block";   
-
-}
 
 
 // <div id='view-playlist-panel'> ... </div>
